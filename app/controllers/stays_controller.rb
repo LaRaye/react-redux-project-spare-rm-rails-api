cff48b74b468a1ef375400d678a4dc765f41d774
LaRaye <lnj2105@gmail.com>
2019-11-09 23:15:24 -0500

    stays controller actions and routes

--- app/controllers/stays_controller.rb
+++ app/controllers/stays_controller.rb
@@ -5,4 +5,45 @@ class StaysController < ApplicationController
     render json: stays
   end
 
+  def show
+     stay = Stay.find(params[:id])
+
+    render json: stay
+  end
+
+  def create
+    stay = Stay.new(stay_params)
+
+    if stay.save
+      render json: stay
+    else
+      render json: {status: stay.errors.full_messages }, status: 422
+    end
+  end
+
+  def update
+    stay = Stay.find(params[:id])
+
+    if stay.update(stay_params)
+      render json: stay
+    else
+      render json: {status: stay.errors.full_messages }, status: 422
+    end
+  end
+
+  def destroy
+    stay = Stay.find(params[:id])
+
+    if stay.destroy
+      render json: stay.title
+    else
+      render json: {status: 500, message: 'Stay cannot be destroyed'}
+    end
+  end
+
+  private
+
+  def stay_params
+    params.require(:stay).permit(:cost, :title, :location)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :hosts, only: [:index, :create, :show, :update, :destroy]
-  resources :stays, only: [:index]
+  resources :stays, only: [:index, :create, :show, :update, :destroy]
   resources :guests, only: [:index, :create, :show, :update, :destroy]
 end
