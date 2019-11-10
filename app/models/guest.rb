35d28ff51759cf0597c4617b62d7332c1c125b68
LaRaye <lnj2105@gmail.com>
2019-11-09 22:02:16 -0500

    validations for host and guest

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,9 @@
 class Guest < ApplicationRecord
   has_many :stays
   has_many :hosts, through: :stays
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :phone, presence: true
 end
--- app/models/host.rb
+++ app/models/host.rb
@@ -5,4 +5,5 @@ class Host < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :phone, presence: true
 end
