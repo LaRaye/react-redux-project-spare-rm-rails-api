643c03752f7735aee2b085ce1431bf43478a3791
LaRaye <lnj2105@gmail.com>
2019-11-09 22:02:24 -0500

    add email to guests table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_10_025727) do
+ActiveRecord::Schema.define(version: 2019_11_10_030050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_11_10_025727) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "phone"
+    t.string "email"
   end
 
   create_table "hosts", force: :cascade do |t|
