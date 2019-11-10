16f46f0e9377be8a27d0025e97be7e04a5ffb9b9
LaRaye <lnj2105@gmail.com>
2019-11-09 22:25:49 -0500

    host controller actions

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,7 +1,49 @@
 class HostsController < ApplicationController
+  before_action :find_host, only: [:show, :update, :destroy]
+
   def index
     hosts = Host.all
 
     render json: hosts
+  end
+
+  def show
+    render json: host
+  end
+
+  def create
+    host = Host.new(host_params)
+
+    if host.save
+      render json: host
+    else
+      render json: {status: host.errors.full_messages }, status: 422
+    end
+  end
+
+  def update
+    if host.update(host_params)
+      render json: host
+    else
+      render json: {status: host.errors.full_messages }, status: 422
+    end
+  end
+
+  def destroy
+    if host.destroy
+      render json: host.full_name
+    else
+      render json: {status: 500, message: 'Host cannot be destroyed'}
+    end
+  end
+
+  private
+
+  def find_host
+    host = Host.find(params[:id])
+  end
+
+  def host_params
+    params.require(:host).permit(:first_name, :last_name, :email, :phone)
   end
 end
--- app/models/host.rb
+++ app/models/host.rb
@@ -6,4 +6,8 @@ class Host < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :phone, presence: true
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
 end
