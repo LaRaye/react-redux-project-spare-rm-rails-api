c7b59ff8ae104434b2d09369f9d566c285caf611
LaRaye <lnj2105@gmail.com>
2019-11-09 22:04:51 -0500

    validations for stay

--- app/models/stay.rb
+++ app/models/stay.rb
@@ -1,4 +1,8 @@
 class Stay < ApplicationRecord
   belongs_to :host
   belongs_to :guest, optional: true
+
+  validates :cost, presence: true
+  validates :title, presence: true
+  validates :location, presence: true
 end
