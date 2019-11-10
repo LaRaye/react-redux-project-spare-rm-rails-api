95975a8e26612ddd9dab18ce29c5ce62c29bade2
LaRaye <lnj2105@gmail.com>
2019-11-09 22:33:37 -0500

    host routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :hosts, only: [:index, :show, :update, :destroy]
+  resources :hosts, only: [:index, :create, :show, :update, :destroy]
   resources :stays, only: [:index]
   resources :guests, only: [:index]
 end
