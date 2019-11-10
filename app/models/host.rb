82b8c3a60c0b010b4b04baf6f6c11d88dc55f44f
LaRaye <lnj2105@gmail.com>
2019-11-09 21:34:35 -0500

    validations for host

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,4 +1,8 @@
 class Host < ApplicationRecord
   has_many :stays, dependent: :destroy
   has_many :guests, through: :stays
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
