2173b5f74545d513f667cc8599910aef5d6650c2
LaRaye <lnj2105@gmail.com>
2019-11-10 20:35:19 -0500

    add nested routes for host-stays and guest-stays

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,15 @@ Rails.application.routes.draw do
   resources :stays, only: [:index, :create, :show, :update, :destroy]
   resources :guests, only: [:index, :create, :show, :update, :destroy]
   resources :users, only: [:index, :create, :show, :update, :destroy]
+
+  Rails.application.routes.draw do
+    resources :hosts, only: [:show, :new, :create] do
+      resources :stays, only: [:show, :index, :new]
+    end
+
+    resources :guests, only: [:show, :new, :create] do
+      resources :stays, only: [:show, :index, :new]
+    end
+  end
+
 end
