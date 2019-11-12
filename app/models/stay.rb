2b2852018f2c5d0f2d04e5c702fb6d5cd956666c
LaRaye <lnj2105@gmail.com>
2019-11-11 22:12:56 -0500

    create custom readers/writers for stay host attributes to also create new host from form data when creating new stay

--- app/models/stay.rb
+++ app/models/stay.rb
@@ -3,20 +3,53 @@ class Stay < ApplicationRecord
   belongs_to :host, optional: true
   belongs_to :guest, optional: true
 
+  after_create :assign_host
+
   validates :cost, presence: true
   validates :title, presence: true
   validates :location, presence: true
 
-  def host_name=(host_name)
-    if !host_name.nil? && host_name != ""
-      first_name = host_name.split(" ")[0]
-      last_name = host_name.split(" ")[1]
-
-      self.host = Host.find_or_create_by(first_name: first_name, last_name: last_name)
+  def host_first_name=(host_first_name)
+    if !host_first_name.nil? && host_first_name != ""
+      @host_first_name = host_first_name
     end
   end
 
-  def host_name
-    self.host ? self.host.full_name : nil
+  def host_first_name
+    self.host ? self.host.first_name : nil
+  end
+
+  def host_last_name=(host_last_name)
+    if !host_last_name.nil? && host_last_name != ""
+      @host_last_name = host_last_name
+    end
+  end
+
+  def host_last_name
+    self.host ? self.host.last_name : nil
+  end
+
+  def host_email=(host_email)
+    if !host_email.nil? && host_email != ""
+      @host_email = host_email
+    end
+  end
+
+  def host_email
+    self.host ? self.host.email : nil
+  end
+
+  def host_phone=(host_phone)
+    if !host_phone.nil? && host_phone != ""
+      @host_phone = host_phone
+    end
+  end
+
+  def host_phone
+    self.host ? self.host.phone : nil
+  end
+
+  def assign_host
+    self.host = Host.find_or_create_by(first_name: @host_first_name, last_name: @host_last_name, email: @host_email, phone: @host_phone)
   end
 end
