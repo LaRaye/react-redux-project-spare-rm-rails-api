cf741e78bf0cdd3720e851aa8cc4ed3fbe8c3ff4
LaRaye <lnj2105@gmail.com>
2019-11-11 22:38:50 -0500

    add uniqueness validation to host email so that only one host instance per email/user

--- app/models/host.rb
+++ app/models/host.rb
@@ -4,7 +4,7 @@ class Host < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :email, uniqueness: true, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :phone, presence: true
 
   def full_name
