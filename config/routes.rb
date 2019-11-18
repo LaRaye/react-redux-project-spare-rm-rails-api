a8c6c8a6101d4b265b78b49bcc492909e9f60d78
LaRaye <lnj2105@gmail.com>
2019-11-17 19:30:22 -0500

    work on routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
 
 
-  resources :users except: [:new, :create]
+  resources :users, only: [:index, :show, :update, :destroy]
   resources :hosts, only: [:index, :create, :show, :update, :destroy]
   resources :stays, only: [:index, :create, :show, :update, :destroy]
   resources :guests, only: [:index, :create, :show, :update, :destroy]
