cd34e2d476816a4519a0160eee19e1ec0ec4f3a0
LaRaye <lnj2105@gmail.com>
2019-11-17 19:21:46 -0500

    build sessions controller, work on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  # get '/home', to: 'sessions#home'
+  get '/', to: 'sessions#create' #home with login form
+  post '/', to: 'sessions#create' #login
+  get '/logout', to: 'sessions#destroy'
+  get '/get_current_user', to: 'sessions#get_current_user'
 
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+
+  resources :users except: [:new, :create]
   resources :hosts, only: [:index, :create, :show, :update, :destroy]
   resources :stays, only: [:index, :create, :show, :update, :destroy]
   resources :guests, only: [:index, :create, :show, :update, :destroy]
